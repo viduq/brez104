de9a2f0276d08f18feb950428e8f2cecba623581
viduq <45742378+viduq@users.noreply.github.com>
2024-02-01 08:49:21 +0100

    Update build_on_windows.bat

--- build/build_on_windows.bat
+++ build/build_on_windows.bat
@@ -1,1 +1,1 @@
-go build -ldflags "-s -w -H=windowsgui -extldflags=-static" -p 4 -v -o brez104.exe ..\cmd\main.go
+go build -ldflags "-s -w -H=windowsgui -extldflags=-static" -p 4 -v -o ..\bin\brez104.exe ..\cmd\main.go
